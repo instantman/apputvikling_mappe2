cbd0569516fba317f09ef5be574c3e74f2a1ed4f
instantman <maddoxx91@gmail.com>
2015-10-11 16:21:48 +0200

    Added overrides and button logic to MainActivity and Register

--- app/src/main/java/s180859_s198527/mappe2/MainActivity.java
+++ app/src/main/java/s180859_s198527/mappe2/MainActivity.java
@@ -1,16 +1,25 @@
 package s180859_s198527.mappe2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    private Button testbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testbutton = (Button)findViewById(R.id.testbutton);
+        testbutton.setOnClickListener(this);
     }
 
     @Override
@@ -34,4 +43,14 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.testbutton:
+                Intent i1 = new Intent(this,Register.class);
+                startActivity(i1);
+                break;
+        }
+    }
+
 }
--- app/src/main/java/s180859_s198527/mappe2/Register.java
+++ app/src/main/java/s180859_s198527/mappe2/Register.java
@@ -1,6 +1,66 @@
 package s180859_s198527.mappe2;
+// Activity to create and store objects in database
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    // Tilbakeknapp i actionbar som sender tilbake til HangMain
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent i = new Intent(getBaseContext(), MainActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                getBaseContext().startActivity(i);
+        }
+        return true;
+    }
+
+    // Tilbakeknapp på telefon, sender tilbak til HangMain
+    @Override
+    public void onBackPressed() {
+        Intent i = new Intent(getBaseContext(), MainActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        getBaseContext().startActivity(i);
+    }
 
 
 
-public class Register {
 }
