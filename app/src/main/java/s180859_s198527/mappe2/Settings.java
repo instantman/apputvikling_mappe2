fa0e1eeb4ba6758126e1d4cdac97a592968c800d
instantman <maddoxx91@gmail.com>
2015-10-12 22:37:13 +0200

    Stuff

--- app/src/main/java/s180859_s198527/mappe2/Settings.java
+++ app/src/main/java/s180859_s198527/mappe2/Settings.java
@@ -1,7 +1,15 @@
 package s180859_s198527.mappe2;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+    }
 
 }
