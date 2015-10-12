087e358089ef2f9d58c2dbe74c771bd927bfa918
Christopher <baugztar@gg.nu>
2015-10-12 12:54:00 +0200

    test fixed holla

--- app/src/main/java/s180859_s198527/mappe2/Contacts.java
+++ app/src/main/java/s180859_s198527/mappe2/Contacts.java
@@ -3,5 +3,4 @@ package s180859_s198527.mappe2;
 import android.support.v7.app.AppCompatActivity;
 
 public class Contacts extends AppCompatActivity {
-    private String testGitTestLolz = "fourtwenty";
 }
