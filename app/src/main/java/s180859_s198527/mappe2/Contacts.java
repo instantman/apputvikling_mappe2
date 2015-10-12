a6fe7e77066bb5faee7ca739922f6a9444767c66
Christopher <baugztar@gg.nu>
2015-10-12 12:49:43 +0200

    oppe i dette

--- app/src/main/java/s180859_s198527/mappe2/Contacts.java
+++ app/src/main/java/s180859_s198527/mappe2/Contacts.java
@@ -3,5 +3,5 @@ package s180859_s198527.mappe2;
 import android.support.v7.app.AppCompatActivity;
 
 public class Contacts extends AppCompatActivity {
-
+    private String testGitTestLolz = "fourtwenty";
 }
