fdda8d3f26f70fcfea9f0d72d62a19262d5807f3
Christopher <baugztar@gg.nu>
2015-10-12 20:57:33 +0200

    nerf

---
+++ app/src/main/java/s180859_s198527/mappe2/Contact.java
@@ -0,0 +1,23 @@
+package s180859_s198527.mappe2;
+
+import java.util.Date;
+
+/**
+ * Created by Christopher on 12/10/2015.
+ */
+public class Contact {
+    private String surname, lastname;
+    private int phoneNr;
+    private Date birthdate;
+
+
+    public Contact(String inSurname, String inLastname, int inPhone, Date inBirthDate){
+        this.surname = inSurname;
+        this.lastname = inLastname;
+        this.phoneNr = inPhone;
+        this.birthdate = inBirthDate;
+    }
+
+
+
+}
