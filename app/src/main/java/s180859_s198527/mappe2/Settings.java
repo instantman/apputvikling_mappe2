4e0c208055f64bf44cfcad1ec2d62d3ea98e6f29
instantman <maddoxx91@gmail.com>
2015-10-19 16:12:01 +0200

    Added separator style. Added switch and separators in Settings. Changed textsize and color in RegContact and Settings.

--- app/src/main/java/s180859_s198527/mappe2/Settings.java
+++ app/src/main/java/s180859_s198527/mappe2/Settings.java
@@ -3,8 +3,17 @@ package s180859_s198527.mappe2;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class Settings extends AppCompatActivity {
+
+    private Switch switch_service;
+    private EditText smstext, smsdate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +24,40 @@ public class Settings extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.mipmap.ic_previous);
 
+        // Knytter switch og tekstfelter til layout
+        switch_service = (Switch)findViewById(R.id.switch_service);
+        smstext = (EditText)findViewById(R.id.textfield_SMStext);
+        smsdate = (EditText)findViewById(R.id.textfield_SMSdate);
+
+        // Setter switch til "ON"
+        switch_service.setChecked(true);
+        // Legger til lytter og funksjonalitet
+        switch_service.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Log.d("Switch", "ON");
+                } else {
+                    Log.d("Switch", "OFF");
+                }
+            }
+        });
     }
+
+    // Metoder for å sette smstext og smsdate
+    public void setSmstext (String inText) {
+        smstext.setText(inText);
+    }
+    public void setSmsdate (String inDate) {
+        smsdate.setText(inDate);
+    }
+
+    // Metoder for å hente smstext og smsdate
+    public String getSmstext() {
+        return smstext.toString();
+    }
+    public String getSmsdate() {
+        return smsdate.toString();
+    }
+
 }
