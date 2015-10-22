6e0792340707b961432ea78816986b418c9408b4
instantman <maddoxx91@gmail.com>
2015-10-22 18:18:17 +0200

    Update Settings.java
    Added comments.

--- app/src/main/java/s180859_s198527/mappe2/Settings.java
+++ app/src/main/java/s180859_s198527/mappe2/Settings.java
@@ -1,3 +1,9 @@
+/** 
+ * This is an activity used to edit settings in the application by the user.
+ * Handles input of SMSText and SMSTime and saves the data to SharedPreferences.
+ * Includes buttons to start and stop SMSService 
+ */
+
 package s180859_s198527.mappe2;
 
 import android.app.Dialog;
@@ -19,12 +25,12 @@ import android.widget.TimePicker;
 import java.util.Calendar;
 
 public class Settings extends AppCompatActivity implements OnClickListener {
-
-    Context context = this;
-
+    
+    Context context = this; /* Sets the activity context */
+    
     private Button btnSave, btnStartService, btnStopService, btnTimupikku;
     private EditText editSMSText;
-
+    /* Initialize SharedPreferences static parameters */
     public static final String SMSPreferences = "SMSPrefs";
     public static final String SMSText = "textKey";
     public static final String SMSTime = "timeKey";
@@ -34,20 +40,22 @@ public class Settings extends AppCompatActivity implements OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        // Oppknapp i ActionBar
+        /* UP-button in ActionBar */
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.mipmap.ic_previous);
-
+        
+        /* Get SharedPreferences and fill text fields */
         SharedPreferences shared = getSharedPreferences("SMSPrefs",MODE_PRIVATE);
         editSMSText = (EditText)findViewById(R.id.textfield_SMSText);
         btnTimupikku = (Button)findViewById(R.id.timupikku);
         editSMSText.setText(shared.getString("textKey", "null"));
         btnTimupikku.setText(shared.getString("timeKey", "null"));
-
+        
         setListener();
     }
-
+    
+    /* Point buttons to UI and add listeners */
     public void setListener() {
         btnTimupikku.setOnClickListener(this);
         btnSave = (Button)findViewById(R.id.button_save);
@@ -57,44 +65,42 @@ public class Settings extends AppCompatActivity implements OnClickListener {
         btnStopService = (Button)findViewById(R.id.button_stopService);
         btnStopService.setOnClickListener(this);
     }
-
+    
+    /* Store current text field data in SharedPreferences  */
     public void setSMSPreferences() {
-        // Get smsText and smsTime
+        /* Get smsText and smsTime */
         String smsText = editSMSText.getText().toString();
         String smsTime = btnTimupikku.getText().toString();
-
-        // Add smsText and smsTime to shared preferences
+        /* Add smsText and smsTime to shared preferences */
         sharedpreferences = getSharedPreferences(SMSPreferences, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedpreferences.edit();
         editor.putString(SMSText, smsText);
         editor.putString(SMSTime, smsTime);
         editor.apply();
-
-        Log.d("SMSTEXT", smsText);
-        Log.d("SMSTIME", smsTime);
     }
-
+    
+    /* Inner class for TimePicker used to store SMSTime */
     public static class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current time as the default values for the picker
+            /* Use current system time as the default values for the picker */
             final Calendar c = Calendar.getInstance();
             int hour = c.get(Calendar.HOUR_OF_DAY);
             int minute = c.get(Calendar.MINUTE);
-
             // Create a new instance of TimePickerDialog and return it
             TimePickerDialog t = new TimePickerDialog(getActivity(),
                     this, hour, minute, DateFormat.is24HourFormat(getActivity()));
             return t;
         }
-
+        /* Listens for user input and sets SMSTime */
         public void onTimeSet(TimePicker view, int setHour, int setMinute) {
             Button b = (Button)getActivity().findViewById(R.id.timupikku);
             String selectedTime = formatTime(setHour,setMinute);
             b.setText(selectedTime);
         }
     }
-
+    
+    /* Function for formatting TimePicker output */
     public static String formatTime(int hour, int minute) {
         String selectedTime;
         if (hour <= 9 && minute <= 9) {
@@ -115,7 +121,7 @@ public class Settings extends AppCompatActivity implements OnClickListener {
         }
     }
 
-    // Håndterer hva som skjer når knapper blir trykket
+    /* Listens for button press and performs related actions */
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.timupikku:
